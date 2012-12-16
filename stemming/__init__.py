2cebf7a0a90cafebb6f51224efae9bb88416bf4a
Peter Organisciak <organis2@illinois.edu>
2012-12-16 17:25:45 -0600

    Code clean-up

--- stemming/__init__.py
+++ stemming/__init__.py
@@ -3,14 +3,11 @@ from nltk.stem.porter import PorterStemmer
 from  nltk.stem.snowball import EnglishStemmer
 from  nltk.stem.wordnet  import WordNetLemmatizer
 '''
- Test stemming for field exam 
+ Example of stemming when it works and when it doesn't. 
 '''
 
 def main():
-    #terms = ["policy", "policies", "police", "retrieval", "retrieved", "retriever"]
-    #terms = ["go", "going", "goed", "went", "swim", "swam", "walked", "walking", "walk", "give", "gave"]
-    #terms = ["geese", "goose", "octopuses", "octopi", "octopodes"]
-    terms = ["batter", "battery", "batteries", "battered"]
+    terms = ["policy", "policies", "police", "retrieval", "retrieved", "retriever", "go", "going", "goed", "went", "swim", "swam", "walked", "walking", "walk", "give", "gave", "geese", "goose", "octopuses", "octopi", "octopodes", "batter", "battery", "batteries", "battered"]
     stemmers = [LancasterStemmer(), PorterStemmer(), EnglishStemmer(ignore_stopwords=False), WordNetLemmatizer()]
     
     print "Stemmer".upper(), "\t", "\t".join(terms).upper()
